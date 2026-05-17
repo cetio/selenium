75af19c55d04438738da2ea6d47a20000335f648
cetio <stakeandtake39@gmail.com>
2026-05-16 23:21:00 -0400

    Add selenium/errors.d: WebDriver exception hierarchy

---
+++ source/selenium/errors.d
@@ -0,0 +1,35 @@
+module selenium.errors;
+
+import std.exception : basicExceptionCtors;
+
+public:
+
+class WebDriverError : Exception
+{
+    mixin basicExceptionCtors;
+}
+
+class NoSuchElementError : WebDriverError
+{
+    mixin basicExceptionCtors;
+}
+
+class StaleElementReferenceError : WebDriverError
+{
+    mixin basicExceptionCtors;
+}
+
+class InvalidElementStateError : WebDriverError
+{
+    mixin basicExceptionCtors;
+}
+
+class WebDriverTimeoutError : WebDriverError
+{
+    mixin basicExceptionCtors;
+}
+
+class WebDriverConnectionError : WebDriverError
+{
+    mixin basicExceptionCtors;
+}
