4f1722d29949f44dfb6d3daf0d86eebe3b586780
cetio <stakeandtake39@gmail.com>
2026-05-16 23:26:00 -0400

    Add selenium/types.d: enums, structs, Capabilities, and LocatorOf template

---
+++ source/selenium/types.d
@@ -0,0 +1,203 @@
+module selenium.types;
+
+import std.json : JSONValue;
+
+import conductor.serialize.json : Name;
+
+public:
+
+enum Browser : string
+{
+    Android = "android",
+    Chrome = "chrome",
+    Firefox = "firefox",
+    HtmlUnit = "htmlunit",
+    InternetExplorer = "internet explorer",
+    IPhone = "iPhone",
+    IPad = "iPad",
+    Opera = "opera",
+    Safari = "safari"
+}
+
+enum Platform : string
+{
+    Windows = "Windows",
+    Xp = "Windows XP",
+    Vista = "Windows Vista",
+    Mac = "Mac",
+    Linux = "Linux",
+    Unix = "Unix",
+    Android = "Android"
+}
+
+enum LocatorStrategy : string
+{
+    ClassName = "class name",
+    CssSelector = "css selector",
+    Id = "id",
+    Name = "name",
+    LinkText = "link text",
+    PartialLinkText = "partial link text",
+    TagName = "tag name",
+    XPath = "xpath"
+}
+
+enum AlertBehaviour : string
+{
+    Accept = "accept",
+    Dismiss = "dismiss",
+    Ignore = "ignore"
+}
+
+enum TimeoutType : string
+{
+    Script = "script",
+    Implicit = "implicit",
+    PageLoad = "page load"
+}
+
+enum MouseButton : int
+{
+    Left = 0,
+    Middle = 1,
+    Right = 2
+}
+
+enum Orientation : string
+{
+    Landscape = "LANDSCAPE",
+    Portrait = "PORTRAIT"
+}
+
+template LocatorOf(string strategy)
+{
+    static if (strategy == "class")
+        enum LocatorStrategy LocatorOf = LocatorStrategy.ClassName;
+    else static if (strategy == "css")
+        enum LocatorStrategy LocatorOf = LocatorStrategy.CssSelector;
+    else static if (strategy == "id")
+        enum LocatorStrategy LocatorOf = LocatorStrategy.Id;
+    else static if (strategy == "name")
+        enum LocatorStrategy LocatorOf = LocatorStrategy.Name;
+    else static if (strategy == "linkText")
+        enum LocatorStrategy LocatorOf = LocatorStrategy.LinkText;
+    else static if (strategy == "partialLinkText")
+        enum LocatorStrategy LocatorOf = LocatorStrategy.PartialLinkText;
+    else static if (strategy == "tag")
+        enum LocatorStrategy LocatorOf = LocatorStrategy.TagName;
+    else static if (strategy == "xpath")
+        enum LocatorStrategy LocatorOf = LocatorStrategy.XPath;
+    else
+        static assert(false, "Unknown locator strategy: " ~ strategy);
+}
+
+struct Capabilities
+{
+    Browser browserName;
+    string browserVersion;
+    Platform platform;
+    bool takesScreenshot;
+    bool handlesAlerts;
+    bool cssSelectorsEnabled;
+    bool javascriptEnabled;
+    bool databaseEnabled;
+    bool locationContextEnabled;
+    bool applicationCacheEnabled;
+    bool browserConnectionEnabled;
+    bool webStorageEnabled;
+    bool acceptSslCerts;
+    bool rotatable;
+    bool nativeEvents;
+    AlertBehaviour unexpectedAlertBehaviour;
+    int elementScrollBehavior;
+
+    @Name("webdriver.remote.sessionid")
+    string remoteSessionId;
+
+    @Name("webdriver.remote.quietExceptions")
+    bool remoteQuietExceptions;
+
+    static Capabilities chrome()
+    {
+        Capabilities ret;
+        ret.browserName = Browser.Chrome;
+        return ret;
+    }
+
+    JSONValue toJSONValue() const
+    {
+        JSONValue ret = JSONValue.emptyObject;
+
+        if (browserName != Browser.init)
+            ret["browserName"] = JSONValue(cast(string)browserName);
+        if (browserVersion.length > 0)
+            ret["browserVersion"] = JSONValue(browserVersion);
+        if (platform != Platform.init)
+            ret["platform"] = JSONValue(cast(string)platform);
+        if (takesScreenshot)
+            ret["takesScreenshot"] = JSONValue(true);
+        if (handlesAlerts)
+            ret["handlesAlerts"] = JSONValue(true);
+        if (cssSelectorsEnabled)
+            ret["cssSelectorsEnabled"] = JSONValue(true);
+        if (javascriptEnabled)
+            ret["javascriptEnabled"] = JSONValue(true);
+        if (databaseEnabled)
+            ret["databaseEnabled"] = JSONValue(true);
+        if (locationContextEnabled)
+            ret["locationContextEnabled"] = JSONValue(true);
+        if (applicationCacheEnabled)
+            ret["applicationCacheEnabled"] = JSONValue(true);
+        if (browserConnectionEnabled)
+            ret["browserConnectionEnabled"] = JSONValue(true);
+        if (webStorageEnabled)
+            ret["webStorageEnabled"] = JSONValue(true);
+        if (acceptSslCerts)
+            ret["acceptSslCerts"] = JSONValue(true);
+        if (rotatable)
+            ret["rotatable"] = JSONValue(true);
+        if (nativeEvents)
+            ret["nativeEvents"] = JSONValue(true);
+        if (unexpectedAlertBehaviour != AlertBehaviour.init)
+            ret["unexpectedAlertBehaviour"] = JSONValue(cast(string)unexpectedAlertBehaviour);
+        if (elementScrollBehavior != 0)
+            ret["elementScrollBehavior"] = JSONValue(elementScrollBehavior);
+
+        return ret;
+    }
+}
+
+struct Size
+{
+    long width;
+    long height;
+}
+
+struct Position
+{
+    long x;
+    long y;
+}
+
+struct Cookie
+{
+    string name;
+    string value;
+    string path;
+    string domain;
+    bool secure;
+    bool httpOnly;
+    long expiry;
+}
+
+struct WebElement
+{
+    string id;
+}
+
+struct LogEntry
+{
+    long timestamp;
+    string level;
+    string message;
+}
