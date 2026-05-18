3b02e66b7adc212ce2c3f9fb4d987f8394e534a7
cetio <stakeandtake39@gmail.com>
2026-05-18 19:08:26 -0400

    Remove page.d

--- source/selenium/package.d
+++ source/selenium/package.d
@@ -4,6 +4,5 @@ public import selenium.driver;
 public import selenium.element;
 public import selenium.errors;
 public import selenium.locator;
-public import selenium.page;
 public import selenium.protocol;
 public import selenium.types;
