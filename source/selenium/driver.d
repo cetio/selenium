f1634a8195cad51632e1d743279ec21f6471e51c
cetio <stakeandtake39@gmail.com>
2026-05-25 00:03:31 -0400

    Sync implicit wait in Driver.findAll

--- source/selenium/driver.d
+++ source/selenium/driver.d
@@ -175,6 +175,8 @@ public:
 
     Element[] findAll(Locator strategy, string value)
     {
+        bridge.ensureImplicitWaitSynced();
+
         JSONValue body_ = JSONValue.emptyObject;
         body_["using"] = cast(string)strategy;
         body_["value"] = value;
