a6b257631eae9cfc7ca3d99811e00f6e9c82b624
cetio <stakeandtake39@gmail.com>
2026-06-04 18:10:00 -0400

    Update package imports

--- source/selenium/package.d
+++ source/selenium/package.d
@@ -1,7 +1,12 @@
 module selenium;
 
 public import selenium.browser;
-public import selenium.bridge;
+public import selenium.browser.chrome;
+public import selenium.browser.firefox;
+
 public import selenium.driver;
+public import selenium.driver.cookies;
+
+public import selenium.bridge;
 public import selenium.element;
 public import selenium.error;
